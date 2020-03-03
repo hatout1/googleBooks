b8e1d1550c5d6291ade1279399c3a59a963af603
hatout01 <hatout1@gmail.com>
2020-03-02 16:50:42 -0800

    added imput form function

--- client/src/Components/SearchForm.js
+++ client/src/Components/SearchForm.js
@@ -1,9 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 
 function SearchForm() {
   return (
     <div>
-      <h1> Hello from SearchForm . </h1>
+      <div id="searchContainer">
+        <h3>Book Search</h3>
+        <form id="bookSearch">
+          <label htmlFor="bookInput" form="bookSearch">
+            Enter a book title:
+          </label>
+          <br></br>
+          <input
+            type="text"
+            name="bookInput"
+            id="bookInput"
+            form="bookSearch"
+            onChange={e => {
+              modifier({ value: e.target.value });
+            }}
+            placeholder="Book Title"
+            required
+          />
+          <br></br>
+          <button
+            type="submit"
+            onClick={e => {
+              e.preventDefault();
+              handleSearchClick();
+            }}
+          >
+            Search!!
+          </button>
+        </form>
+        <div>
+          <Results />
+        </div>
+      </div>
     </div>
   );
 }
