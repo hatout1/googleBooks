44de0607b6b6b34465aaa5993b9a4abd50d69387
hatout01 <hatout1@gmail.com>
2020-03-03 02:49:37 -0800

    track heroku nodmon error

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ mongoose.connect(MONGO_URI, {
 });
 
 if (process.env.MONGO_URI === "production") {
-  app.use(express.static("/client/build"));
+  app.use(express.static("client/build"));
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
