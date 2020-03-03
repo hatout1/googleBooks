cef4f64b5c936a1800df473de1372cf962a6daef
hatout01 <hatout1@gmail.com>
2020-03-02 17:03:21 -0800

    added axios function to get the data from google Api

--- client/src/Components/SearchForm.js
+++ client/src/Components/SearchForm.js
@@ -1,6 +1,34 @@
+import axios from "axios";
 import React, { useState } from "react";
 
+import Results from "./Results";
+
 function SearchForm() {
+  const [value, modifier] = useState({ value: "" });
+  const [book, booksModifier] = useState({ books: [] });
+
+  function handleSearchClick() {
+    let url = `https://www.googleapis.com/books/v1/volumes?q=${value.value}`;
+
+    axios.get(url).then(res => {
+      console.log(res);
+      let booksData = res.data.items;
+      let allBooks = [];
+      for (let i = 0; i < booksData.length; i++) {
+        let book = {};
+        let info = booksData[i].volumeInfo;
+        book["id"] = booksData[i].id;
+        book["title"] = info.title;
+        book["authors"] = info.authors;
+        book["description"] = info.description;
+        book["image"] = info.imageLinks.thumbnail;
+        book["link"] = info.infoLink;
+        allBooks.push(book);
+      }
+      booksModifier({ books: [allBooks] });
+    });
+  }
+
   return (
     <div>
       <div id="searchContainer">
